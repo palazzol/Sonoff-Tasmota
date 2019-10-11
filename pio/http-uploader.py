3a47ad63cd709b5418e50587ded3bcce25b2f9d6
Frank Palazzolo <frank@avoidspikes.com>
2019-10-11 14:47:06 -0400

    Make http-uploader.py platform-independant

--- pio/http-uploader.py
+++ pio/http-uploader.py
@@ -1,3 +1,5 @@
+import os
+
 Import("env")
 
 # pio < 4.0.0
@@ -7,11 +9,7 @@ Import("env")
 # env.Replace(UPLOADCMD="$UPLOADER -u " + b64decode(ARGUMENTS.get("UPLOAD_PORT")) + " -f $SOURCES")
 
 # pio >= 4.0.0
-env.Replace(UPLOADER="pio\espupload.py")
+env.Replace(UPLOADER="pio"+os.sep+"espupload.py")
 env.Replace(UPLOADERFLAGS="")
 env.Replace(UPLOADCMD="$UPLOADER -u $UPLOAD_PORT -f $SOURCES")
 
-'''
-env.Replace(UPLOADCMD="pio\espupload.py -f $SOURCES") # Windows
-env.Replace(UPLOADCMD="pio/espupload.py -f $SOURCES") # Linux
-'''
